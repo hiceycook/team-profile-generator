3bc882c88ba72c2d3f4b158bf1627c02b8951f5a
hicey <hiceycook@gmail.com>
2021-09-10 13:42:50 -0400

    output file working

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const fs = require('fs');
 const path = require('path');
 const genHTML = require('./src/generate-html');
 const { prompt } = require('inquirer');
+const { FS } = require('jest-haste-map');
 const teamArr = [];
-const OUTPUT_DIR = path.resolve(__dirname, 'output');
-const outputPath = path.join(OUTPUT_DIR, 'team-profile.html');
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team-profile.html")
 
 
 //command line prompt using inquirer //
@@ -72,10 +73,292 @@ const promptUser = () => {
         //TAKE INPUT ANSWERS AN CREATE A NEW MANAGER OBJECT//
     ])
         .then(answers => {
-            console.log(answers);
+            // console.log(answers);
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNo);
+            //PUSH NEW MANAGER OBJECT TO TEAM ARRAY //
+            teamArr.push(manager);
+            //CALL NEXT PROMPT //
+            promptTeamBuilder();
+        })
+};
+//TEAM BUILDER INQUIRER PROMPT //
+const promptTeamBuilder = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'team',
+            message: 'Select an option:',
+            choices: [
+                'Add manager',
+                'Add engineer',
+                'Add intern',
+                'Complete team profile'
+            ]
         }
-
-        )
+    ])
+        //FUNCTION TO HANDLE USER CHOICE SELECTION //
+        .then(choice => {
+            switch (choice.team) {
+                case 'Add manager':
+                    addManager();
+                    break;
+                case 'Add engineer':
+                    addEngineer();
+                    break;
+                case 'Add intern':
+                    addIntern();
+                    break;
+                default:
+                    completeTeam();
+            }
+        });
 };
 
+//ADD MANAGER, ENGINEER, AND INTERN FUNCTIONS CALLED BY CHOICE SELECTION //
+const addManager = () => {
+    console.log(`
+    =================
+    adding manager...
+    =================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter the manager's name",
+            validate: engineerName => {
+                if (engineerName) {
+                    return true
+                } else {
+                    console.log('Please enter a name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: 'Enter their id number',
+            // Check if user entered something
+            validate: id => {
+                if (id) {
+                    return true
+                } else {
+                    console.log('Please enter an id number');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: 'Enter their email',
+            // Check if user entered something
+            validate: email => {
+                if (email) {
+                    return true
+                } else {
+                    console.log('Please enter an email');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "officeNo",
+            message: 'Enter their office number',
+            // Check if user entered something
+            validate: officeNo => {
+                if (officeNo) {
+                    return true
+                } else {
+                    console.log('Please enter an office number');
+                    return false;
+                }
+            }
+        }
+    ]).then(answers => {
+        //CREATE NEW MANAGER OBJECT //
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNo);
+        //ADD NEW MANAGER TO TEAM ARRAY //
+        teamArr.push(manager);
+        // RETURN TO THE TEAM CREATION //
+        promptTeamBuilder();
+    });
+};
+
+const addEngineer = () => {
+    console.log(`
+    ==================
+    adding engineer...
+    ==================
+    `)
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter the engineer's name",
+            // Check if user entered something
+            validate: engineerName => {
+                if (engineerName) {
+                    return true
+                } else {
+                    console.log('Please enter a name');
+                    return false;
+                }
+            }
+
+        },
+
+        {
+            type: "input",
+            name: "id",
+            message: 'Enter their id number:',
+            // Check if user entered something
+            validate: id => {
+                if (id) {
+                    return true
+                } else {
+                    console.log('Please enter an id number');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: 'Enter their email:',
+            // Check if user entered something
+            validate: email => {
+                if (email) {
+                    return true
+                } else {
+                    console.log('Please enter an email');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: "input",
+            name: "github",
+            message: 'Enter their github username:',
+            // Check if user entered something
+            validate: github => {
+                if (github) {
+                    return true
+                } else {
+                    console.log('Please enter the username as it appears on github');
+                    return false;
+                }
+            }
+        }
+    ]).then(answers => {
+        // console.log(answers);
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        // Pushes the new manager object to the team array
+        teamArr.push(engineer);
+        // Call menu
+        promptTeamBuilder();
+    });
+};
+
+const addIntern = () => {
+    console.log(`
+    ================
+    adding intern...
+    ================
+    `)
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter the intern's name",
+            // Check if user entered something
+            validate: internName => {
+                if (internName) {
+                    return true
+                } else {
+                    console.log('Please enter a name');
+                    return false;
+                }
+            }
+
+        },
+
+        {
+            type: "input",
+            name: "id",
+            message: 'Enter their id number:',
+            // Check if user entered something
+            validate: id => {
+                if (id) {
+                    return true
+                } else {
+                    console.log('Please enter an id number');
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: 'Enter their email:',
+            // Check if user entered something
+            validate: email => {
+                if (email) {
+                    return true
+                } else {
+                    console.log('Please enter an email');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: "input",
+            name: "school",
+            message: "Enter the intern's school:",
+            // Check if user entered something
+            validate: school => {
+                if (school) {
+                    return true
+                } else {
+                    console.log('Please enter the school name');
+                    return false;
+                }
+            }
+        }
+    ]).then(answers => {
+        // console.log(answers);
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        // Pushes the new manager object to the team array
+        teamArr.push(intern);
+        // Call menu
+        promptTeamBuilder();
+    });
+};
+
+const completeTeam = () => {
+    console.log(
+        `
+    ====================
+    Team Profile Created
+    ====================
+    `
+    );
+
+    //OUTPUT FUNCTION//
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdir(OUTPUT_DIR);
+    };
+    fs.writeFileSync(outputPath, genHTML(teamArr), 'utf-8');
+};
+
+
+
+
+
+
+//RUN APP//
 promptUser();
---
+++ src/generate-html.js
@@ -0,0 +1,99 @@
+const index = require('../index');
+
+const createTeamProfile = (team) => {
+    const html = [];
+
+    const createManagerHTML = manager => {
+        console.log(manager);
+        let managerHTML = `
+    <div class="member-container">
+        <div class="member-header">
+        <h1>Manager</h1>
+        <h2>Name: ${manager.name}</h2>
+        </div>
+        <div class="member-information-container">
+            <h3>ID: ${manager.id}</h3>
+            <h3>E-Mail: <a href="mailto:${manager.email}">${manager.email}</a></h3>
+            <h3>Office #: ${manager.officeNo}</h3>
+        </div>
+    </div>
+    `;
+        html.push(managerHTML);
+    }
+
+    const createEngineerHTML = engineer => {
+        console.log(engineer);
+        let engineerHTML = `
+    <div class="member-container">
+        <div class="member-header">
+        <h1>Engineer</h1>
+        <h2>Name: ${engineer.name}</h2>
+        </div>
+        <div class="member-information-container">
+            <h3>ID: ${engineer.id}</h3>
+            <h3>E-Mail: <a href="mailto:${engineer.email}">${engineer.email}</a></h3>
+            <h3>Github: <a target="_blank" href="https://github.com/${engineer.github}">${engineer.github}</a></h3>
+        </div>
+    </div>
+    `;
+        html.push(engineerHTML);
+    };
+
+    const createInternHTML = intern => {
+        console.log(intern);
+        let internHTML = `
+        <div class="member-container">
+            <div class="member-header">
+            <h1>Intern</h1>
+            <h2>Name: ${intern.name}</h2>
+            </div>
+            <div class="member-information-container">
+                <h3>ID: ${intern.id}</h3>
+                <h3>E-Mail: <a href="mailto:${intern.email}">${intern.email}</a></h3>
+                <h3>School: ${intern.school}</h3>
+            </div>
+        </div>
+        `;
+        html.push(internHTML);
+    };
+
+    for (let i = 0; i < team.length; i++) {
+        if (team[i].getRole() === "Manager") {
+            createManagerHTML(team[i]);
+        }
+        if (team[i].getRole() === "Engineer") {
+            createEngineerHTML(team[i]);
+        }
+        if (team[i].getRole() === "Intern") {
+            createInternHTML(team[i]);
+        }
+    };
+
+    // Combine everything and return to be exported
+    return html.join('');
+}
+
+
+
+module.exports = team => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="../assets/style.css">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <header>
+            <h1> My Team </h1>
+        </header>
+        
+        <main>${createTeamProfile(team)}</main>
+    </body>
+    </html>`
+};
